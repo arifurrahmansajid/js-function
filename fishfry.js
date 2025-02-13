e53411c1767ae0cb8b4275265fae5b0696f50779
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2025-02-13 12:55:05 +0600

    Add files via upload

---
+++ fishfry.js
@@ -0,0 +1,13 @@
+function firshFry(mach, shobji, mosla){
+    console.log('Mach:', mach);
+    console.log('Shobji:', shobji);
+    console.log('Mosla:', mosla);
+
+    console.log(mach + "Date");
+
+
+    return mach + shobji + mosla+"Ready to eat";
+}
+
+const result = firshFry("Rui", "Potol", "Lanka");
+console.log('Result:', result);
